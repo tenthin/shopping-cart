ead497136b78ca4f3fc24979b469ae208d8a7273
Tenzin <tenthinten@gmail.com>
2022-01-18 19:05:21 +0530

    display that cart is empty

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import Header from './components/Header'
 import Main from './components/Main'
 import Basket from './components/Basket'
 import data from './data'
+import { useState } from 'react'
 
 
 function App() {
   const {products} = data  //using constracting assignment to extract products form data//
+  const [cartItems, setCartItems] = useState([])
+  
   return (
     <div className="App">
       <Header></Header>
       <div className='row'>
         {/* pass products to main component */}
         <Main products={products}></Main>
-        <Basket></Basket>
+        <Basket cartItems={cartItems}></Basket>
       </div>
     </div>
   );
