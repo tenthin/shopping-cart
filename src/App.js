dfd2641822438eb0e5c2a2a2ff5fbf6b7b884bd8
Tenzin <tenthinten@gmail.com>
2022-01-19 13:34:43 +0530

    remove items from cart

--- src/App.js
+++ src/App.js
@@ -21,13 +21,26 @@ function App() {
     }
   } 
 
+  const onRemove = (product) => {
+    const exist=cartItems.find((x) => x.id === product.id);
+    if(exist.qty === 1){
+      setCartItems(cartItems.filter((x) => x.id !== product.id))
+    }else{
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? {...exist, qty: exist.qty - 1 }: x
+        )
+      )
+     }
+  }
+
   return (
     <div className="App">
       <Header></Header>
       <div className='row'>
         {/* pass products to main component */}
         <Main onAdd={onAdd} products={products}></Main>
-        <Basket onAdd={onAdd} cartItems={cartItems}></Basket>
+        <Basket onAdd={onAdd} onRemove={onRemove} cartItems={cartItems}></Basket>
       </div>
     </div>
   );
