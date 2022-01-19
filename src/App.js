1b4bce9daeced441c2a84585b0ccaee6b0cbdeaa
Tenzin <tenthinten@gmail.com>
2022-01-19 15:30:31 +0530

    shows number of items in cart

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header></Header>
+      <Header countCartItems={cartItems.length}></Header>
       <div className='row'>
         {/* pass products to main component */}
         <Main onAdd={onAdd} products={products}></Main>
